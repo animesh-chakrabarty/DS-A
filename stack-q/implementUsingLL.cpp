0f9e32b7e0594846e46c07e5ef80d75f78134c1f
animesh-chakrabarty <animesh_chakrabarty@outlook.com>
2024-08-07 15:39:41 +0530

    added: implementation of queue using LL

--- stack-q/implementUsingLL.cpp
+++ stack-q/implementUsingLL.cpp
@@ -12,6 +12,7 @@ public:
 	}
 };
 
+// Implementation of stack 
 class StackImp{
 public:
 	int top = -1;
@@ -63,13 +64,86 @@ public:
 		return mover->data;
 	}
 
-	// size
+	// size - return size of the stack
 	int size(){
 		return top+1;
 	}
 };
 
+// Implementation of Queue
+class QueueImp{
+public:
+	Node* frontptr = nullptr;
+	Node* backptr = nullptr;
+	int length = 0;
+
+	// push - insert element
+	void push(int value){
+		Node* temp = new Node(value);
+
+		// if queue is empty
+		if(length == 0){
+			frontptr = temp;
+			backptr = temp;
+			length++;
+			return;
+		}
+
+		// if queue is not empty
+		backptr->next = temp;
+		backptr = temp;
+		length++;
+		return;
+	}
+
+	// pop - remove element from front 
+	void pop(){
+		// if queue is empty
+		if(length == 0){
+			cout << "no element inside queue to remove"<< endl;
+			return;
+		}
+		// if queue is not empty
+		frontptr = frontptr->next;
+
+		length--;
+		return;
+	}
+
+	// front - returns the front element 
+	int front(){
+		if(length == 0){
+			cout << "queue is empty" << endl;
+			return -1;
+		}
+
+		return frontptr->data;
+	}
+
+	// back - returns the back element
+	int back(){
+		if(length == 0){
+			cout << "queue is empty" << endl;
+			return -1;
+		}
+
+		return backptr->data;
+	}
+
+	// size - returns the size of the queue
+	int size(){
+		return length;
+	}
+
+	// empty - checks is stack is empty or not
+	bool empty(){
+		return length == 0;
+	}
+};
+
 int main(){
+
+	// Stack Operations 
 	StackImp* st = new StackImp();
 	st->push(10);
 	st->push(11);
@@ -87,19 +161,27 @@ int main(){
 
 	cout << endl;
 
-	// QueueImp* q = new QueueImp();
-	// q->push(10);
-	// q->push(11);
-	// q->push(12);
-	// cout << q->size() << " ";
-	// q->pop();
-	// cout << q->topElement() << " ";
-	// q->push(90);
-	// cout << q->topElement() << " ";
-	// q->pop();
-	// cout << q->size() << " ";
-	// cout << q->topElement() << " ";
+	// Queue Operations 
+	QueueImp* q = new QueueImp();
+	q->push(10);
+	q->push(11);
+	q->push(12);
+	cout << q->front() << " ";
+	cout << q->back() << " ";
+	cout << q->size() << " ";
+	q->pop();
+	cout << q->front() << " ";
+	cout << q->back() << " ";
+	q->push(90);
+	cout << q->front() << " ";
+	cout << q->back() << " ";
+	cout << q->size() << " ";
+	q->pop();
+	cout << q->size() << " ";
+	cout << q->front() << " ";
+	cout << q->back() << " ";
 
-	// delete q;
+	delete q;
+
 	return 0;
 }
