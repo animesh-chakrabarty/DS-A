f626323f8234e50ed371c836e6a32eaf25d0a66f
animesh-chakrabarty <animesh_chakrabarty@outlook.com>
2024-09-30 16:00:09 +0530

    added: postOrder-iterative-2stack second approach

--- Tree/postOrder-iterative-2stack.cpp
+++ Tree/postOrder-iterative-2stack.cpp
@@ -28,7 +28,8 @@ Node* createBT(){
 	return root;
 }
 
-vector<int> postOrder(Node* root){
+// post order traversal using 2 stack (storing values in vector)
+vector<int> postOrder2(Node* root){
 	stack<Node*> st1;
 	stack<Node*> st2;
 	vector<int> ans;
@@ -56,6 +57,28 @@ vector<int> postOrder(Node* root){
 	return ans;
 }
 
+// post order traversal using 1 stack (storing values in vector)
+vector<int> postOrder(Node* root){
+	stack<Node*> st1;
+	vector<int> ans;
+
+	st1.push(root);
+
+	while(!st1.empty()){
+		Node* temp = st1.top();
+		st1.pop();
+
+		ans.push_back(temp->data);
+
+		if(temp->left)st1.push(temp->left);
+		if(temp->right)st1.push(temp->right);
+	}
+
+	reverse(ans.begin(), ans.end());
+
+	return ans;
+}
+
 int main(){
 	Node* root = createBT();
 
@@ -63,4 +86,11 @@ int main(){
 	ans = postOrder(root);
 
 	for(auto it: ans)cout << it << " ";
+
+	cout << endl;
+
+	vector<int> ans2;
+	ans2 = postOrder2(root);
+
+	for(auto it: ans2)cout << it << " ";
 }
