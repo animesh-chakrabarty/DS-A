5c7d117bb5342678a243ce4447e94d350287f498
animesh-chakrabarty <animesh_chakrabarty@outlook.com>
2024-10-05 15:30:37 +0530

    minor change

--- Tree/Views/top.cpp
+++ Tree/Views/top.cpp
@@ -22,7 +22,8 @@ Node* createBT(){
 	root->left->left = new Node(4);
 	root->left->right = new Node(5);
 
-	root->right->left = new Node(6);
+	root->left->right->left = new Node(6);
+
 	root->right->right = new Node(7);
 
 	return root;
